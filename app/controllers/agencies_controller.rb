793b8e443b3d1d10480d3f9d1a46a99d0bd05407
ccases <carlosmatthewcases@gmail.com>
2022-02-09 10:29:38 +0800

    resolved conflicts

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,4 +1,12 @@
 class AgenciesController < ApplicationController
   def index
+    @agencies = Agency.all
+  end
+
+  def destroy
+    authenticate_admin!
+    @agency = User.find_by(agency_id: params[:id])
+    @agency.destroy
+    redirect_to admin_users_path(user_type: "agency"), flash: {success: "Agency destroyed"}
   end
 end
