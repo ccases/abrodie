3513e23b2a4281b4aea08687f136f45f8c0fce60
ccases <carlosmatthewcases@gmail.com>
2022-02-09 10:38:34 +0800

    removed destroy from agencies controller, transferred to users controller instead

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -3,10 +3,4 @@ class AgenciesController < ApplicationController
     @agencies = Agency.all
   end
 
-  def destroy
-    authenticate_admin!
-    @agency = User.find_by(agency_id: params[:id])
-    @agency.destroy
-    redirect_to admin_users_path(user_type: "agency"), flash: {success: "Agency destroyed"}
-  end
 end
