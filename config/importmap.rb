e77bc62b669d359b2f781350f697b5cb96546e46
ccases <carlosmatthewcases@gmail.com>
2022-02-10 20:18:49 +0800

    working flash messages for login stuff

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+# pin "materialize-css", to: "https://ga.jspm.io/npm:materialize-css@1.0.0/dist/js/materialize.js"
+# pin "@materializecss/materialize", to: "https://ga.jspm.io/npm:@materializecss/materialize@1.1.0-alpha/dist/js/materialize.js"
