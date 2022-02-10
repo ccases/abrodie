c96cc44f902b89232896a599f084663231c705d2
Patrick Ranchez <ranchezpatrick@gmail.com>
2022-02-10 15:14:07 +0800

    fixed routes for application

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+
+    def update 
+    end
+
+    def applications_params
+    end
+
+    def set_applications
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,10 @@ Rails.application.routes.draw do
     get "/:user_type/sign_up", to: "users/registrations#new", as: "new_user"
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :jobs
+  resources :jobs do
+    resources :applications
+  end
+  
   resources :pages
   # Defines the root path route ("/")
   root to: "pages#index"
