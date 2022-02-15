86612792df5278b6ab60eecff85e9100c00b1ad3
annora <annora@LAPTOP-624LMDFD.localdomain>
2022-02-15 20:01:39 +0800

    added config for assets in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # setting to true
+  config.serve_static_assets = true
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
